7d7600ebd4320f6917c388085585eed50f803723
Shubham Kumar <shubh199815@gmail.com>
2022-06-25 07:17:58 +0530

    Initial commit 2

---
+++ pages/collections/[collectionId].jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+
+const Collection = () => {
+  const router = useRouter();  
+  return (
+    <div></div>
+  )
+}
+
+export default Collection
